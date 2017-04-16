e356d6d9242f651b3875925c8998a24c9c44e7e6
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2017-04-15 20:34:14 -0400

    fixed boilerplate code

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello"
+import { Hello } from "./components/Hello";
 
-ReactDOM.render {
-	<Hello compiler="TypeScript" framework="React"/>
-};
+ReactDOM.render(
+    <Hello compiler="TypeScript" framework="React" />,
+    document.getElementById("example")
+);
